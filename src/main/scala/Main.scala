d1e18252193d8f35d2facc4146f2a3fd9ca15328
Stan97-2 <stan.dellon972@gmail.com>
2023-07-03 14:51:20 +0200

    read json file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,24 @@
-package sudoku
-
 import zio._
+import zio.Console._
+import zio.json._
+import java.nio.charset.StandardCharsets
+import zio.nio.file.{Path, Files}
 
 object Main extends ZIOAppDefault {
+
+  def readJsonFile(path: String): ZIO[Any, Throwable, String] =
+    Files
+      .readAllBytes(Path(path))
+      .map(bytes => new String(bytes.toArray, StandardCharsets.UTF_8))
+
 
   def run: ZIO[Any, Throwable, Unit] =
     for {
       _ <- Console.print("Enter the path to the JSON file containing the Sudoku problem:")
       path <- Console.readLine
       _ <-  Console.printLine(s"You entered: $path")
+      jsonFile <- readJsonFile(path)
+      _ <-  Console.printLine(jsonFile)
       // Add your Sudoku solver logic here, utilizing ZIO and interacting with the ZIO Console
     } yield ()
 }
